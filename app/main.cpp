5eee50d806c4e5b457b7e325d02eee8c2742cd71
Constanza González <44975847+ConstanzaG@users.noreply.github.com>
2019-05-05 20:48:06 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,14 @@
+/* cabecera de las llamadas a MPI */
+#include </usr/include/openmpi/mpi.h> 
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /* cabecera de las llamadas a MPI */
-#include <mpi.h> 
+//#include <mpi.h> 
 
+//me dijeron que use MPI_Get_processor_name
 /**
  * Manda un mensaje desde todos los procesos con
  * rango distinto de 0 al proceso 0. El proceso 0
@@ -16,12 +20,21 @@
 int main(int argc, char** argv) {
     int mi_rango; /* rango del proceso    */
     int p; /* numero de procesos   */
+    int len; /*largo del nombre de la maquina*/
     int fuente; /* rango del que envia  */
     int dest; /* rango del que recibe */
     int tag = 0; /* etiqueta del mensaje */
-    char mensaje[100]; /* mensaje  */
+    char mensaje[300]; /* mensaje  */
+    char name[50]; /*nombre de la maquina*/
     MPI_Status estado; /* devuelve estado al recibir*/
-
+    
+    /* obtener la fecha actual */
+    time_t fecha = time(0);
+    struct tm *tlocal = localtime(&fecha);
+    char tiempo[128];
+    strftime(tiempo,128,"%Y/%m/%d %H:%M:%S",tlocal);
+    
+    
     /* Comienza las llamadas a MPI */
     MPI_Init(&argc, &argv);
 
@@ -35,7 +48,8 @@ int main(int argc, char** argv) {
 
     if (mi_rango != 0) {
         /* Crea mensaje */
-        sprintf(mensaje, "Saludos del proceso %d!", mi_rango);
+        MPI_Get_processor_name(name,&len);
+        sprintf(mensaje, "%s %s", name,tiempo);
         dest = 0;
         /* Usa strlen+1 para que la marca /0 se transmita */
         MPI_Send(mensaje, strlen(mensaje) + 1, MPI_CHAR,
